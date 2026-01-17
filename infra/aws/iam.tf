4e5f3936af8ec83511170921deb4f4fa6ed8367e
JSON <manamana32321@gmail.com>
2026-01-17 23:50:41 +0900

    feat(infra): add IAM user and policy for managing Route53 DNS records

--- infra/aws/iam.tf
+++ infra/aws/iam.tf
@@ -48,3 +48,49 @@ output "github_actions_secret_access_key" {
   value     = aws_iam_access_key.github_actions.secret
   sensitive = true
 }
+
+# IAM user for skkuding/lab repository to manage Route53 DNS records
+resource "aws_iam_user" "lab_route53" {
+  name = "skkuding-lab-route53"
+}
+
+resource "aws_iam_user_policy" "lab_route53" {
+  name = "skkuding-lab-route53-policy"
+  user = aws_iam_user.lab_route53.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:GetHostedZone",
+          "route53:ListResourceRecordSets",
+          "route53:ChangeResourceRecordSets",
+          "route53:GetChange"
+        ]
+        Resource = [
+          aws_route53_zone.main.arn,
+          "arn:aws:route53:::change/*"
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "route53:ListHostedZones"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "lab_route53" {
+  user = aws_iam_user.lab_route53.name
+}
+
+output "lab_route53_access_key_id" {
+  value = aws_iam_access_key.lab_route53.id
+}
+
+output "lab_route53_secret_access_key" {
+  value     = aws_iam_access_key.lab_route53.secret
+  sensitive = true
+}
