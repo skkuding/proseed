c84ed2912fada938b7c4b00df2bf3d18f5fd07bb
Clover229 <jeonsumin2002@gmail.com>
2026-02-20 17:22:10 +0900

    fix(fe): modify next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  transpilePackages: ['@mdxeditor/editor'],
-  reactStrictMode: true,
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config
-  },
 }
 
 export default nextConfig
