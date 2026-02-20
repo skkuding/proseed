1986abfeb009b90e6902c500bbbdf66fbdfb19b3
Clover229 <jeonsumin2002@gmail.com>
2026-02-21 00:33:19 +0900

    chore(fe): make prohect page

---
+++ apps/web/src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,8 @@
+export function ProjectPage() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-head0_sb_52">ProjectPage</h1>
+      <p className="text-caption4_r_12">hello</p>
+    </main>
+  )
+}
