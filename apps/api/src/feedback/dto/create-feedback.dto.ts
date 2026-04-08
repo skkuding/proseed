8993b7838a02d28c0b22d1c2bc811adc9b3766c5
EddieDawn <jinu@EddieDawnui-MacBookPro.local>
2026-04-08 13:20:00 +0900

    feat: create-feedback DTO

--- apps/api/src/feedback/dto/create-feedback.dto.ts
+++ apps/api/src/feedback/dto/create-feedback.dto.ts
@@ -1,1 +1,42 @@
-export class CreateFeedbackDto {}
+import { Type } from 'class-transformer'
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator'
+
+export class FeedbackItemDto {
+  @IsNumber()
+  @IsNotEmpty()
+  questionId!: number
+
+  @IsString()
+  @IsNotEmpty()
+  content!: string
+
+  @IsOptional()
+  @IsString()
+  imageURL?: string
+}
+
+export class CreateFeedbackDto {
+  @IsNumber()
+  @IsNotEmpty()
+  projectId!: number
+
+  @IsNumber()
+  @IsNotEmpty()
+  versionId!: number
+
+  @IsString()
+  @IsNotEmpty()
+  onelineReview!: string
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => FeedbackItemDto)
+  feedbacks!: FeedbackItemDto[]
+}
