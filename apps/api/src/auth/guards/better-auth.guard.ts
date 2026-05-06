b99127ae780aa5a3378187c9c5ba3bf11ed34af1
EddieDawn <jinu@EddieDawnui-MacBookPro.local>
2026-05-06 19:59:16 +0900

    feat: implemented finaAllQuestions API

--- apps/api/src/auth/guards/better-auth.guard.ts
+++ apps/api/src/auth/guards/better-auth.guard.ts
@@ -6,16 +6,12 @@ import {
   UnauthorizedException,
 } from '@nestjs/common'
 import { fromNodeHeaders } from 'better-auth/node'
-import { ConfigService } from '@nestjs/config'
 import type { RequestWithUser } from 'src/common/types/request-with-user.type'
 
 @Injectable()
 export class BetterAuthGuard implements CanActivate {
-  constructor(
-    private readonly betterAuthService: BetterAuthService,
-    private readonly configService: ConfigService,
-  ) {}
-
+  constructor(private readonly betterAuthService: BetterAuthService) {}
+  //요청 헤더에서 세션 쿠키 존재하는지 확인
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest<RequestWithUser>()
     const session = await this.betterAuthService.auth.api.getSession({
@@ -24,13 +20,13 @@ export class BetterAuthGuard implements CanActivate {
 
     if (!session) throw new UnauthorizedException()
 
-    // 세션 유효하면 req.user에 유저 id 주입
-    const userIdInNumber = Number(session.user.id) // DB의 user.id를 number로 형변환
+    //세션 유효하면 req.user에 유저 id
+    const userIdInNumber = Number(session.user.id) //DB의 user.id를 number로 형변환
     if (isNaN(userIdInNumber)) {
-      // 반환값이 NaN인지 확인
+      //반환값이 NAN인지 확인
       throw new UnauthorizedException('Invalid userId')
     }
-    req.user = { id: userIdInNumber } // req 객체에 user 프로퍼티 추가해주기
+    req.user = { id: userIdInNumber } //req 객체에 user 프로퍼티 추가해주기
     return true
   }
 }
