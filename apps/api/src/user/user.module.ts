5e3742bd980cfb60de1b26aad3895cf8801fde80
EddieDawn <jinu@EddieDawnui-MacBookPro.local>
2026-05-06 20:08:21 +0900

    chore: deleted duplicated import in user.module.ts

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from 'src/auth/auth.module'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
-import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
   imports: [AuthModule],
