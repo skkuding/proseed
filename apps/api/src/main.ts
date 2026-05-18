64c14e15fb3fe896185ba1998d345495e5865aa9
zero1177 <younghur07@gmail.com>
2026-05-18 14:43:11 +0900

    fix(be): fix cors setting in main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,10 +14,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const betterAuthService = app.get(BetterAuthService)
 
+  app.enableCors()
   //소셜 로그인 callback, 세션 조회 등 better-auth의 모든 요청은 /api/auth경로에 마운트
   app.use('/api/auth', toNodeHandler(betterAuthService.auth)) //미들웨어 단계라서 아래 라우트 글로벌 설정 전에 적용됨. (충돌 X)
   app.setGlobalPrefix('api')
-  app.enableCors()
   await app.listen(process.env.PORT ?? 4000)
 }
-bootstrap()
+void bootstrap()
