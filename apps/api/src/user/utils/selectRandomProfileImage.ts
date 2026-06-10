5680058d3f126beeebe5e352d230e2cdcb7ee246
zero1177 <younghur07@gmail.com>
2026-06-10 22:50:10 +0900

    chore(be): fix minor issues

--- apps/api/src/user/utils/selectRandomProfileImage.ts
+++ apps/api/src/user/utils/selectRandomProfileImage.ts
@@ -1,15 +1,16 @@
+const PROFILE_IMAGES = [
+  '/profile_avocado.svg',
+  '/profile_cake.svg',
+  '/profile_fish.svg',
+  '/profile_fortunecookie.svg',
+  '/profile_juice.svg',
+  '/profile_maple.svg',
+  '/profile_onikiri.svg',
+  '/profile_salmonsushi.svg',
+  '/profile_shrimp.svg',
+  '/profile_tunasushi.svg',
+] as const
+
 export function selectRandomProfileImage(): string {
-  const PROFILEIMAGES = [
-    '/profile_avocado.svg',
-    '/profile_cake.svg',
-    '/profile_fish.svg',
-    '/profile_fortunecookie.svg',
-    '/profile_juice.svg',
-    '/profile_maple.svg',
-    '/profile_onikiri.svg',
-    '/profile_salmonsushi.svg',
-    '/profile_shrimp.svg',
-    '/profile_tunasushi.svg',
-  ] as const
-  return PROFILEIMAGES[Math.floor(Math.random() * PROFILEIMAGES.length)]
+  return PROFILE_IMAGES[Math.floor(Math.random() * PROFILE_IMAGES.length)]
 }
