4cb0c509798239d027dd300523531b6e0e93ea4a
zero1177 <younghur07@gmail.com>
2026-06-12 18:11:22 +0900

    fix(be): add IsOptional to myPageUpdateDto

--- apps/api/src/user/dto/mypageUpdate.dto.ts
+++ apps/api/src/user/dto/mypageUpdate.dto.ts
@@ -1,22 +1,30 @@
 import { JobType } from '@prisma/client'
-import { IsArray, IsEnum, IsString } from 'class-validator'
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator'
 
 export class MypageUpdateDto {
   @IsString()
+  @IsOptional()
   name?: string
 
   @IsEnum(JobType)
+  @IsOptional()
   jobType?: JobType
 
   @IsString()
+  @IsOptional()
   profileImageUrl?: string
 
   @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
   skills?: string[]
 
   @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
   links?: string[]
 
   @IsString()
+  @IsOptional()
   bio?: string
 }
