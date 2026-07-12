54b73c3e49ddcce5932b2e12b23a14fd2d3d3cc7
zero1177 <younghur07@gmail.com>
2026-07-13 02:29:37 +0900

    feat(be): add counting logics for participatingProject & myfeedback

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -40,25 +40,6 @@ export class UserService {
     }
   }
 
-  //다른 유저의 공개 프로필 조회 (프로젝트 상세 - 함께한 팀원용, 민감 정보 제외)
-  async getUserProfile(userId: number): Promise<UserProfileResponseDto> {
-    const user = await this.prisma.user.findUnique({
-      where: { id: userId },
-      select: {
-        id: true,
-        name: true,
-        profileImageUrl: true,
-        jobType: true,
-        skills: true,
-        links: true,
-        bio: true,
-      },
-    })
-    if (!user) throw new EntityNotExistException('User')
-
-    return user
-  }
-
   /**
    * 유저의 신규/기존 회원 여부를 판별합니다.
    * @param {number} userId 유저 아이디
@@ -84,44 +65,57 @@ export class UserService {
   }
 
   /**
-   * 유저의 프로필 정보들을 조회합니다.
+   * 유저 본인의 프로필 정보들을 조회합니다.
    *
    * @param userId 유저 아이디
    * @returns 마이페이지 화면 표시 정보가 담긴 user 객체
    */
-  async getProfile(userId: number) {
-    const user = await this.prisma.user.findUnique({
-      where: { id: userId },
-      select: {
-        name: true,
-        email: true,
-        accounts: {
+  async getMyProfile(userId: number) {
+    const [user, participatingProjectCount, myFeedbackCount] =
+      await Promise.all([
+        this.prisma.user.findUnique({
+          where: { id: userId },
           select: {
-            providerId: true,
+            name: true,
+            email: true,
+            accounts: {
+              select: {
+                providerId: true,
+              },
+            },
+            jobType: true,
+            profileImageUrl: true,
+            skills: true,
+            links: true,
+            bio: true,
+            ownedTicketCount: true,
           },
-        },
-        jobType: true,
-        profileImageUrl: true,
-        skills: true,
-        links: true,
-        bio: true,
-        ownedTicketCount: true,
-      },
-    })
+        }),
+        this.prisma.projectRole.count({
+          where: { userId },
+        }),
+        this.prisma.feedbackSubmission.count({
+          where: { userId },
+        }),
+      ])
 
     if (!user) throw new NotFoundException('User not found')
 
-    return user
+    return {
+      ...user,
+      participatingProjectCount,
+      myFeedbackCount,
+    }
   }
 
   /**
-   * 유저의 마이페이지 수정사항을 업데이트합니다.
+   * 유저 본인의 마이페이지 수정사항을 업데이트합니다.
    *
    * @param {number} userId 유저아이디
    * @param {MypageUpdateDto} mypageUpdateDto
    * @returns 정보가 업데이트된 유저 객체
    */
-  async updateProfile(userId: number, mypageUpdateDto: MypageUpdateDto) {
+  async updateMyProfile(userId: number, mypageUpdateDto: MypageUpdateDto) {
     const { name, jobType, profileImageUrl, skills, links, bio } =
       mypageUpdateDto
 
@@ -152,6 +146,42 @@ export class UserService {
         }
       }
       throw error
+    }
+  }
+
+  /**
+   * 다른 유저의 공개 프로필을 조회합니다. (프로젝트 상세 - 함께한 팀원용, 민감한 정보 제외)
+   * @param userId number
+   * @returns
+   */
+  async getOtherProfile(userId: number): Promise<UserProfileResponseDto> {
+    const [user, participatingProjectCount, feedbackCount] = await Promise.all([
+      this.prisma.user.findUnique({
+        where: { id: userId },
+        select: {
+          id: true,
+          name: true,
+          jobType: true,
+          profileImageUrl: true,
+          skills: true,
+          links: true,
+          bio: true,
+        },
+      }),
+      this.prisma.projectRole.count({
+        where: { userId },
+      }),
+      this.prisma.feedbackSubmission.count({
+        where: { userId },
+      }),
+    ])
+
+    if (!user) throw new EntityNotExistException('User')
+
+    return {
+      ...user,
+      participatingProjectCount,
+      feedbackCount,
     }
   }
 }
