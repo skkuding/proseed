69cb867c12421acc17f31b7369a0e8d51c8674c9
Young Hur <younghur07@gmail.com>
2026-07-12 15:00:28 +0900

    chore(be): add ! for satisfying strictPropertyInitialization (#79)

--- apps/api/src/user/dto/user-response.dto.ts
+++ apps/api/src/user/dto/user-response.dto.ts
@@ -3,34 +3,34 @@ import { JobType, UserRole } from '@prisma/client'
 
 export class UserCheckResponseDto {
   /** 온보딩(jobType 설정) 미완료 여부 */
-  isNewUser: boolean
+  isNewUser!: boolean
 
-  nickname: string
+  nickname!: string
 }
 
 export class NicknameResponseDto {
-  nickname: string
+  nickname!: string
 }
 
 export class UserResponseDto {
-  id: number
-  name: string
-  email: string
-  emailVerified: boolean
-  profileImageUrl: string
-  createdAt: Date
-  updatedAt: Date
-  ownedTicketCount: number
+  id!: number
+  name!: string
+  email!: string
+  emailVerified!: boolean
+  profileImageUrl!: string
+  createdAt!: Date
+  updatedAt!: Date
+  ownedTicketCount!: number
 
   @ApiProperty({ enum: JobType, enumName: 'JobType', nullable: true })
-  jobType: JobType | null
+  jobType!: JobType | null
 
   @ApiProperty({ enum: UserRole, enumName: 'UserRole' })
-  userRole: UserRole
+  userRole!: UserRole
 
-  skills: string[]
-  links: string[]
+  skills!: string[]
+  links!: string[]
 
   @ApiProperty({ nullable: true, type: String })
-  bio: string | null
+  bio!: string | null
 }
